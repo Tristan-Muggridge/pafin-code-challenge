2e6148dad004b886fc611e1048d7d7a29890a7db
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-08-18 22:54:57 +1000

    Update user.ts
    adding new routes to-do

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,10 +1,35 @@
 import { Request, Response } from "express";
 import db from "../db";
+import httpCodes from "../enums/httpCodes";
 
-export default {
-    async getAll(req: Request, res: Response) {
-        const users = await db.user.findMany();
-        // if (users.length == 0) throw new Error("No users found");   
-        res.json(users);
+interface Controller {
+    [key: string]: (req: Request, res: Response) => void
+}
+
+class JSONResponse {
+    status: number;
+    message?: string;
+    data: Object;
+
+    constructor(status: number, data: Object, message?:string) {
+        this.status = status;
+        this.data = {...data};
+        if (message) this.message = message;
     }
 }
+
+const controller: Controller = {
+    getAll: async (req: Request, res: Response) => {
+        const users = await db.user.findMany();
+        const message = users.length === 0 ? "No users found." : "";
+        const response = new JSONResponse(httpCodes.Ok, {users}, message);
+        res.json(response);
+    },
+
+    getOne: async (req: Request, res: Response) => new Error("Not implemented"),
+    create: async (req: Request, res: Response) => new Error("Not implemented"),
+    update: async (req: Request, res: Response) => new Error("Not implemented"),
+    delete: async (req: Request, res: Response) => new Error("Not implemented"),
+}
+
+export default controller;
