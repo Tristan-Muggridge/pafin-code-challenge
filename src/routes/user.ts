f523e23983fc04c285304110c742ae1cfdfde1d1
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-08-18 22:55:05 +1000

    Update user.ts
    include new routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,6 +3,13 @@ import controller from "../controllers/user";
 
 const router = Router();
 
-router.get('/', controller.getAll);
+router.route('/')
+    .get(controller.getAll)
+    .post(controller.create)
+
+router.route('/:id')
+    .get(controller.getOne)
+    .put(controller.update)
+    .delete(controller.delete)
 
 export default router;
