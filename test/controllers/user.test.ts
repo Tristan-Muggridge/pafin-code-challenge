6844e5682b9223b4549ec5d0426be9448ae378c4
Tristan M・ﾄリスタン <89533155+Tristan-Muggridge@users.noreply.github.com>
2023-08-18 23:54:06 +1000

    Update user.test.ts
    Implement testing for validation functions for user create route

--- test/controllers/user.test.ts
+++ test/controllers/user.test.ts
@@ -3,10 +3,162 @@ import { describe } from 'mocha';
 import request from 'supertest';
 import app from '../../src/app';
 
-describe('GET /api/user', () => {
+describe('GET /api/user', async () => {
     it('Should return a status of 200', async () => {
         const result = await request(app).get('/api/user');
         expect(result.status).to.equal(200);
-        expect(result.body).to.be.an('array');
-    })
+    });
+
+    it('Should return an array of users', async () => {
+        const result = await request(app).get('/api/user');
+        expect(result.body.data.users).to.be.an('array');
+    });
 })
+
+import { validateName, validateEmail, validatePassword } from '../../src/controllers/user';        
+
+describe('ValidateName', () => {
+
+    it('Stephen is a valid name', () => {
+        const result = validateName('Stephen');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: true,
+        });
+    });
+
+    it('St is not a valid name', () => {
+        const result = validateName('St');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: false,
+        });
+    });
+
+    it('"" is not a valid name', () => {
+        const result = validateName('');
+        expect(result).to.deep.equal({
+            provided: false,
+            length: false,
+        });
+    });
+
+    it('undefined is not a valid name', () => {
+        const result = validateName(undefined);
+        expect(result).to.deep.equal({
+            provided: false,
+            length: false,
+        });
+    });
+
+});
+
+describe('ValidateEmail', () => {
+    
+    it('test.user@test.com is a valid email', () => {
+        const result = validateEmail('test.user@test.com');
+        expect(result).to.deep.equal({
+            provided: true,
+            valid: true,
+        });
+    });
+
+    it('test.user@test is not a valid email', () => {
+        const result = validateEmail('test.user@test');
+        expect(result).to.deep.equal({
+            provided: true,
+            valid: false,
+        });
+    });
+
+    it('"" is not a valid email', () => {
+        const result = validateEmail('');
+        expect(result).to.deep.equal({
+            provided: false,
+            valid: false,
+        });
+    });
+
+    it('undefined is not a valid email', () => {
+        const result = validateEmail(undefined);
+        expect(result).to.deep.equal({
+            provided: false,
+            valid: false,
+        });
+    });
+
+});
+
+describe('ValidatePassword', () => {
+
+    it('Password1! is a valid password', () => {
+        const result = validatePassword('Password1!');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: true,
+            number: true,
+            special: true,
+        });
+    });
+
+    it('Password1 is not a valid password - no special', () => {
+        const result = validatePassword('Password1');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: true,
+            number: true,
+            special: false,
+        });
+    });
+
+    it('Password! is not a valid password - no number', () => {
+        const result = validatePassword('Password!');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: true,
+            number: false,
+            special: true,
+        });
+    });
+
+    it('Password is not a valid password - no num & no special', () => {
+        const result = validatePassword('Password');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: true,
+            number: false,
+            special: false,
+        });
+    });
+
+    it('test is not a valid password - too short, no num, no special', () => {
+        const result = validatePassword('Password');
+        expect(result).to.deep.equal({
+            provided: true,
+            length: true,
+            number: false,
+            special: false,
+        });
+    });
+
+    it('"" is not a valid password - blank', () => {
+        const result = validatePassword('');
+        expect(result).to.deep.equal({
+            provided: false,
+            length: false,
+            number: false,
+            special: false,
+        });
+    });
+
+    it('undefined is not a valid password - undefined', () => {
+        const result = validatePassword(undefined);
+        expect(result).to.deep.equal({
+            provided: false,
+            length: false,
+            number: false,
+            special: false,
+        });
+    });
+
+});
